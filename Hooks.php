7d2744679decfd6eb306f34471078f88686c8f58
sphynkx <sfynkx@gmail.com>
2024-11-09 01:50:27 +0200

    first commit

---
+++ Hooks.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MediaWiki\Extension\HideActions;
+
+class Hooks implements \MediaWiki\Hook\BeforePageDisplayHook {
+
+	/**
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/BeforePageDisplay
+	 * @param \OutputPage $out
+	 * @param \Skin $skin
+	 */
+	public function onBeforePageDisplay( $out, $skin ): void {
+
+	    ## The 'raw' doesnt block by this funtion. Need to set `$wgActions['raw'] = false;` in LocalSettings.php
+	    $blockedactions = array('raw', 'delete', 'revert', 'rollback', 'protect', 'unprotect', 'markpatrolled', 'deletetrackback', 'edit', 'blockdiff', 'submit');
+	    $action = $out->getActionName();
+
+	    if ( $out->getUser()->mId == 0 and in_array( $action, $blockedactions) ) {
+		$out->prepareErrorPage( $out->getTitle(), false ) ;
+		$out->addHTML( \Html::element( 'h2', [], wfMessage( 'hideactions-xsdenied' )->text() ) );
+	    }
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+[Mediawiki](https://www.mediawiki.org/) extension for [Veda.Wiki](https://veda.wiki/) project. Disable possibility to view wiki source of pages for unauthorized users.
+## Install
+* Dowload and unpack to Mediawiki's `extensions/` directory.
+* Set in `LocalSettings.php`
+
+    `wfLoadExtension( 'CatList' );`
+    `$wgActions['raw'] = false;`
